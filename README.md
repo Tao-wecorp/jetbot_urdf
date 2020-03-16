201af47e61775fbc246e0d63bae99b882df515a0
boringtao <xinghui.tao@wecorpindustries.com>
2020-03-16 12:53:10 +0000

    add person sim

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 ## To-do
 1. Add Jetbot SDF file to gazebo library;
 2. Launch Jetbot URDF in ROS;
-3. Launch worlds in ROS.
+3. Launch worlds in ROS;
+4. Control Jetbot in Gazebo;
+5. Add training algorithms.
 
 ## Env
 1. Run ./env.sh
--- src/jetbot_gazebo/scripts/twist.py
+++ src/jetbot_gazebo/scripts/twist.py
@@ -5,11 +5,11 @@ from std_msgs.msg import Float64
 import time
 
 rospy.init_node('jetbot')
-rate = rospy.Rate(2000)
+rate = rospy.Rate(30)
 pub_vel_left_1 = rospy.Publisher('/robot/joint1_velocity_controller/command', Float64, queue_size=5)
 pub_vel_right_1 = rospy.Publisher('/robot/joint2_velocity_controller/command', Float64, queue_size=5)
 
 while not rospy.is_shutdown():
     pub_vel_left_1.publish(30)
-    pub_vel_right_1.publish(30)
+    pub_vel_right_1.publish(0)
 rate.sleep()
