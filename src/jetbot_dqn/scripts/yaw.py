d93f1856b4b8494acacac27b9c02f2ae65043085
boringtao <xinghui.tao@wecorpindustries.com>
2020-04-07 10:46:09 +0100

    Add: reset simulation

--- src/jetbot_dqn/scripts/yaw.py
+++ src/jetbot_dqn/scripts/yaw.py
@@ -39,10 +39,10 @@ class Pose(object):
 
         self.states_sub = rospy.Subscriber("/gazebo/model_states",ModelStates,self.states_callback)
         self.set_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)    
-        self.reset_simulation = rospy.ServiceProxy('/gazebo/reset_simulation', Empty)
+        # self.reset_simulation = rospy.ServiceProxy('/gazebo/reset_simulation', Empty)
+        # self.reset_simulation()
 
         rate = rospy.Rate(30)
-        self.reset_simulation()
         state_robot_msg = ModelState()
         state_robot_msg.model_name = 'robot'
         while not rospy.is_shutdown():
@@ -66,11 +66,12 @@ class Pose(object):
                 for i in range(len(points)):
                     if points[i] is not None:
                         frame = cv2.circle(frame, (int(points[i][0]), int(points[i][1])), 3, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
+                # frame = cv2.circle(frame, (int(x_hip), int(y_hip)), 3, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
                 frame = cv2.circle(frame, (int(x_fpv), int(y_fpv)), 10, (255, 0, 255), thickness=-1, lineType=cv2.FILLED)
                 cv2.imshow("", frame)
                 cv2.waitKey(1)
 
-                # print("%s seconds" % (time.time() - start_time))
+                print("%s seconds" % (time.time() - start_time))
                 time.sleep(round((time.time() - start_time), 1))
             rate.sleep()
     
